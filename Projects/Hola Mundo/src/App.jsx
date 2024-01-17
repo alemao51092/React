aa04906085aeef4def6aac368447cd0b99a9197a
Alejandro Martinez <aarielmartinez1111@gmail.com>
2024-01-17 16:40:24 -0300

    Project React

---
+++ Projects/Hola Mundo/src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Heraldo',
+    isFollowing: false
+  },
+  {
+    userName: 'Paco Hdez',
+    name: 'paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+       users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+          key={userName}  
+          userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        
+       ))
+      }
+    </section>
+  )
+}
